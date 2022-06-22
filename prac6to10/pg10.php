f451ee6afdd43f6b1782572435653cc3d9d5d2c7
harshithva <harshith11032001@gmail.com>
2022-06-22 09:45:07 +0530

    prac 6 to 10

---
+++ prac6to10/pg10.php
@@ -0,0 +1,84 @@
+<?php
+ $servername = "localhost";
+ $username = "root";
+ $password = "";
+ $dbname = "weblab";
+ $conn = mysqli_connect($servername, $username, $password, $dbname);
+ if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+ }
+ $sql = "SELECT * FROM student";
+ $result = $conn->query($sql); 
+ $usn = array() ;
+ echo "<table border='1'><caption>Before Sorting </caption><br>";
+ echo "<tr><th>USN</th><th>NAME</th><th>Marks</th></tr>"; 
+ if ($result->num_rows > 0)
+ {
+  while($row = $result->fetch_assoc())
+  { 
+   echo "<tr><td>". $row["usn"]."</td>";
+   echo "<td>". $row["name"]."</td>";
+   echo "<td>". $row["marks"]."</td></tr>";
+   $usn[] = $row["usn"] ;
+  }
+ }
+ $n = sizeof($usn) ;
+ for($i = 0 ; $i < $n-1 ; $i++ )
+ {
+  $pos = $i ;
+  for($j = $i + 1 ; $j < $n ; $j++ )
+  {
+   if( $usn[$pos] < $usn[$j])
+   {
+    $pos = $j ;
+   }
+  }
+  if( $pos != $i)
+  {
+   $temp = $usn[$i] ;
+   $usn[$i] = $usn[$pos] ;
+   $usn[$pos] = $temp ;
+  }
+ }
+ $name = [] ;
+ $marks = [] ;
+ $result = $conn->query($sql);
+ if ($result->num_rows> 0)
+ {
+  while($row = $result->fetch_assoc()) 
+  {
+   for($i=0;$i<$n;$i++) 
+   {
+    if($row["usn"] == $usn[$i]) 
+    {
+     $name[$i]=$row["name"];
+     $marks[$i]=$row["marks"];
+    }
+   }
+  }
+ }
+ echo "<br><table border='1'><caption>After Sorting </caption><br>";
+ echo "<tr><th>USN</th><th>NAME</th><th>Marks</th></tr>"; 
+ for($i = 0 ; $i < sizeof($usn) ; $i++)
+ {
+  echo "<tr><td>". $usn[$i]."</td>";
+  echo "<td>". $name[$i]."</td>";
+  echo "<td>". $marks[$i]."</td></tr>";
+ }
+?>
+
+<?php
+
+// CREATE TABLE `student` (
+//   `usn` varchar(20),
+//   `name` varchar(20),
+//   `marks` int(11)
+// );
+
+// INSERT INTO `student` (`usn`, `name`, `marks`) VALUES
+// ('4JK19CS020', 'Harshith', 100),
+// ('4JK19CS028', 'Lk', 87),
+// ('4JK19CS009', 'Abhishek', 97)
+
+
+?>
---
+++ prac6to10/pg7.php
@@ -0,0 +1,17 @@
+<head>
+ <meta http-equiv="refresh" content="1"/>
+ <style>
+  p {
+   color:white;
+   font-size:90px;
+   position:absolute;
+   top: 40%;
+   left: 50%;
+   transform: translate(-50%, -50%);
+  }
+  body {
+   background-color:#284387;
+  }
+ </style>
+ <p> <?php echo date(" h: i : s A");?> </p>
+</head>
